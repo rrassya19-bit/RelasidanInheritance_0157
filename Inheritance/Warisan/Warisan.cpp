b14eecdecdc33b2c9e92f7a202717427b46b9285
rrassya19-bit <rrassys19@gmail.com>
2026-05-11 15:44:48 +0700

    konstruktor kelas pelajar

--- Inheritance/Warisan/Warisan.cpp
+++ Inheritance/Warisan/Warisan.cpp
@@ -21,6 +21,11 @@ public:
 class pelajar : public orang {
 public:
     string sekolah;
+
+    pelajar(string pNama, string pSekolah) :
+        orang(pNama), sekolah(pSekolah) {
+        cout << "pelajar dibuat\n" << endl;
+    }
 };
 
 int main() {
