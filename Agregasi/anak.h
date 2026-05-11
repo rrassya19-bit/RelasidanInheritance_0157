6404ba72667c2bf3c7c9570b8de6c0d01e93c453
rrassya19-bit <rrassys19@gmail.com>
2026-05-11 15:14:21 +0700

    destruktor kelas anak

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -5,10 +5,17 @@
 using namespace std;
 
 class anak {
+    
 public:
     string nama;
-    anak(string pNama) :nama(pNama) {
+    anak(string pNama) :nama(pNama)
+    {
         cout << "Anak \"" << nama << "\" ada\n";
+    }
+
+    ~anak()
+    {
+        cout << "Anak \"" << nama << "\" tidak ada\n";
     }
 };
 
