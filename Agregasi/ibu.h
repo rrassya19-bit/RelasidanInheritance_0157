144e2e2eca790642d0fb4b53b8b6b730ce37e8dc
rrassya19-bit <rrassys19@gmail.com>
2026-05-11 15:16:08 +0700

    konstruktor dan destruktor kelas ibu

--- Agregasi/ibu.h
+++ Agregasi/ibu.h
@@ -7,10 +7,16 @@
 using namespace std;
 
 class ibu {
-    
 public:
     string nama;
     vector<anak*> daftar_anak;
+
+    ibu(string pNama) :nama(pNama) {
+        cout << "Ibu \"" << nama << "\" ada\n";
+    }
+    ~ibu() {
+        cout << "Ibu \"" << nama << "\" tidak ada\n";
+    }
 };
 
 #endif
