b7b16d67b0fbe97194bbdc430f2c34f913421a94
rrassya19-bit <rrassys19@gmail.com>
2026-05-11 15:25:28 +0700

    konstruktor dan destruktor kelas pasien

--- Asosiasi/asosiasi.cpp
+++ Asosiasi/asosiasi.cpp
@@ -8,6 +8,12 @@ class pasien { // asosiasi
 public:
     string nama;
     vector<dokter*> daftar_dokter;
+    pasien(string pNama) :nama(pNama) {
+        cout << "Pasien \"" << nama << "\" ada\n";
+    }
+    ~pasien() {
+        cout << "Pasien \"" << nama << "\" tidak ada\n";
+    }
 };
 
 int main() {
